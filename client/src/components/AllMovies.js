76dec3d4667e1de2c24bdd00004bb2fb4b3dbc96
Alina Yakauleva <alina.yakauleva@gmail.com>
2021-12-22 12:33:20 -0600

    added search box

--- client/src/components/AllMovies.js
+++ client/src/components/AllMovies.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@reach/router';
+import axios from 'axios';
 
 const AllMovies = (props) => {
-    const {movies, submitHandler} = props;
+    const {movies} = props;
+    const [searchWord, setSearchWord] = useState('');
+    const [searchResults, setSearchResult] = useState(false);
     
+    const search = (e) => {
+        console.log(searchWord);
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=76e97ee4b0b9bbd10b8a54f5b87265c0&query=${searchWord}`)
+            .then(res => {
+                console.log(res.data.results);
+                setSearchResult(res.data.results);
+            })
+            console.log(searchResults);
+    }
+
     return (
         <div className='wrapper' style={{display: 'block'}}>
             <div className='search'>
-                <p>Search By Category</p>
-                <select>
-                    <option value='' defaultValue >Select</option>
-                    <option value= 't'>Title</option>
-                    <option value='genre'>Genre</option>
-                    <option></option>
-                </select>
-                <button className='searchButton' onClick={submitHandler}>Search</button>
+                <p>Search By Title</p>
+                <input className='searchInput' type='text' placeholder='Search' onChange={(e) => setSearchWord(e.target.value)} />
+                <button className='searchButton' onClick={search}>Search</button>
             </div>
+            {
+                searchResults
+                ? <div>
+                    <h1 style={{textAlign: 'center', margin: '30px 0px'}}>Search Results</h1>
+                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                        {searchResults.map((result, index) => {
+                            return (
+                                <div key={index} className='oneMovie'>
+                                    <Link to={`/movies/${result.id}`} style={{color: 'black', textDecoration: 'none'}}>
+                                        <div style={{fontWeight: 'bold'}}>{result.title}</div>
+                                        <img src={`http://image.tmdb.org/t/p/w500/${result.poster_path}`} alt='image' />
+                                    </Link>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                : null
+            }
+            <h1 style={{textAlign: 'center', margin: '30px 0px'}}>Popular Movies</h1>
             <div style={{display: 'flex', flexWrap: 'wrap'}}>
                 {movies.map((movie, index) => {
                     return (
